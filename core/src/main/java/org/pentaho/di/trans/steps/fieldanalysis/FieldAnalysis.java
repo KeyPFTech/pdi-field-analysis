a7721c90eba4653e8ebf3574d85932755879d13e
adamfowleruk <adamfowleruk@gmail.com>
2018-01-17 11:58:39 +0000

    Partial fix for #3

--- core/src/main/java/org/pentaho/di/trans/steps/fieldanalysis/FieldAnalysis.java
+++ core/src/main/java/org/pentaho/di/trans/steps/fieldanalysis/FieldAnalysis.java
@@ -78,6 +78,16 @@ public class FieldAnalysis extends BaseStep implements StepInterface {
     datePatternMap.put( "MM/dd/yyyy", Pattern.compile( "(0?[1-9]|1[0-2])/[0123]?[0-9]/[0-9][0-9][0-9][0-9]" ) );
     datePatternMap.put( "dd/MM/yyyy HH:mm", Pattern.compile( "[0123]?[0-9]/(0?[1-9]|1[0-2])/[0-9][0-9][0-9][0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]" ) );
     datePatternMap.put( "MM/dd/yyyy HH:mm", Pattern.compile( "(0?[1-9]|1[0-2])/[0123]?[0-9]/[0-9][0-9][0-9][0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]" ) );
+    datePatternMap.put( "yyyy-MM-dd HH:mm:ss", Pattern.compile( "[0-9][0-9][0-9][0-9]-(0?[1-9]|1[0-2])-[0123]?[0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9]" ) );
+    datePatternMap.put( "MM/dd/yyyy HH:mm:ss", Pattern.compile( "(0?[1-9]|1[0-2])/[0123]?[0-9]/[0-9][0-9][0-9][0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9]" ) );
+    datePatternMap.put( "MM/dd/yyyy HH:mm:ss.SSS", Pattern.compile( "(0?[1-9]|1[0-2])/[0123]?[0-9]/[0-9][0-9][0-9][0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9]\\.[0-9][0-9][0-9]" ) );
+    datePatternMap.put( "MM/dd/yyyy HH:mm:ss.SSS X", Pattern.compile( "(0?[1-9]|1[0-2])/[0123]?[0-9]/[0-9][0-9][0-9][0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9]\\.[0-9][0-9][0-9] [+\\-][0-9][0-9]:?[0-9][0-9]" ) );
+    datePatternMap.put( "MM/dd/yyyy HH:mm:ss X", Pattern.compile( "(0?[1-9]|1[0-2])/[0123]?[0-9]/[0-9][0-9][0-9][0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9] [+\\-][0-9][0-9]:?[0-9][0-9]" ) );
+    
+    datePatternMap.put( "yyyy/MM/dd HH:mm:ss", Pattern.compile( "[0-9][0-9][0-9][0-9]/(0?[1-9]|1[0-2])/[0123]?[0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9]" ) );
+    datePatternMap.put( "yyyy/MM/dd HH:mm:ss X", Pattern.compile( "[0-9][0-9][0-9][0-9]/(0?[1-9]|1[0-2])/[0123]?[0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9] [+\\-][0-9][0-9]:?[0-9][0-9]" ) );
+    datePatternMap.put( "yyyy/MM/dd HH:mm:ss.SSS X", Pattern.compile( "[0-9][0-9][0-9][0-9]/(0?[1-9]|1[0-2])/[0123]?[0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9]\\.[0-9][0-9][0-9] [+\\-][0-9][0-9]:?[0-9][0-9]" ) );
+    datePatternMap.put( "yyyy/MM/dd HH:mm:ss.SSS", Pattern.compile( "[0-9][0-9][0-9][0-9]/(0?[1-9]|1[0-2])/[0123]?[0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9]\\.[0-9][0-9][0-9]" ) );
   }
 
   private synchronized boolean isNumber( String val ) {
@@ -378,8 +388,8 @@ public class FieldAnalysis extends BaseStep implements StepInterface {
 
           // NB this check is here as a single check for any date pattern is quicker than a set of specific date patterns
           //    Overall execution will be slower if most fields are dates, faster otherwise
-          // TODO make this more definitive, and a Pattern and Matcher. E.g. matches string with / or - for dates
-          if ( val.contains( "/" ) ) {
+          // TODO make this more definitive, add a Pattern and Matcher. E.g. matches string with / or - for dates
+          if ( val.contains( "/" ) || val.contains("-") || val.contains(":") ) {
 
             keySet = (Set<String>) datePatternMap.keySet();
             keyIter = keySet.iterator();
