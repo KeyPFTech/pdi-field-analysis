b6fb0d2fba69c6ee497dcbb58e178e1961ee7093
adamfowleruk <adamfowleruk@gmail.com>
2018-01-18 16:38:31 +0000

    Added another date format

--- core/src/main/java/org/pentaho/di/trans/steps/fieldanalysis/FieldAnalysis.java
+++ core/src/main/java/org/pentaho/di/trans/steps/fieldanalysis/FieldAnalysis.java
@@ -88,6 +88,8 @@ public class FieldAnalysis extends BaseStep implements StepInterface {
     datePatternMap.put( "yyyy/MM/dd HH:mm:ss X", Pattern.compile( "[0-9][0-9][0-9][0-9]/(0?[1-9]|1[0-2])/[0123]?[0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9] [+\\-][0-9][0-9]:?[0-9][0-9]" ) );
     datePatternMap.put( "yyyy/MM/dd HH:mm:ss.SSS X", Pattern.compile( "[0-9][0-9][0-9][0-9]/(0?[1-9]|1[0-2])/[0123]?[0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9]\\.[0-9][0-9][0-9] [+\\-][0-9][0-9]:?[0-9][0-9]" ) );
     datePatternMap.put( "yyyy/MM/dd HH:mm:ss.SSS", Pattern.compile( "[0-9][0-9][0-9][0-9]/(0?[1-9]|1[0-2])/[0123]?[0-9] ([0-1][0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9]\\.[0-9][0-9][0-9]" ) );
+    
+    datePatternMap.put( "MM/dd/yyyy-HH:mm:ss", Pattern.compile( "(0?[1-9]|1[0-2])/[0123]?[0-9]/[0-9][0-9][0-9][0-9]-([0-1][0-9]|2[0-4]):[0-5][0-9]:[0-5][0-9]" ) );
   }
 
   private synchronized boolean isNumber( String val ) {
